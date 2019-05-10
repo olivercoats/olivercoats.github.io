81a698cf9632ca26df3d9945b8462dc10abc8f38
Oliver Coats <oliver@tynedalewebsites.co.uk>
2019-05-10 21:10:14 +0100

    Now with colour chart

---
+++ colour-chart/js/colour-chart.js
@@ -0,0 +1,9 @@
+function lightBackground(){
+  document.getElementById('switching').style.backgroundImage = "radial-gradient(#ffffff, #e4e4e4)";
+  document.getElementById('title').style.color = "#1a3d5a";
+}
+
+function darkBackground(){
+  document.getElementById('switching').style.backgroundImage = "radial-gradient(#1a3d5a, #0c1f2f)";
+  document.getElementById('title').style.color = "#fff";
+}
