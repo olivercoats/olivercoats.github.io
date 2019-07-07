7d211910bf69fc5a30c5aaea7ea2a180de101db5
Oliver Coats <oliver@tynedalewebsites.co.uk>
2019-07-07 17:23:46 +0100

    Moved colour-chart to own repository

--- projects/colour-chart/js/colour-chart.js
+++
@@ -1,9 +0,0 @@
-function lightBackground(){
-  document.getElementById('switching').style.backgroundImage = "radial-gradient(#ffffff, #e4e4e4)";
-  document.getElementById('title').style.color = "#1a3d5a";
-}
-
-function darkBackground(){
-  document.getElementById('switching').style.backgroundImage = "radial-gradient(#1a3d5a, #0c1f2f)";
-  document.getElementById('title').style.color = "#fff";
-}
